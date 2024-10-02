53e860ee1366afdcb98e368b98875eacce47e139
Patrick Girardi <dev.patrickgirardi@gmail.com>
2024-10-02 17:05:04 -0300

    docs: started documentation

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoute from "./src/routes/user.route.js";
 import authRoute from "./src/routes/auth.route.js";
 import newsRoute from "./src/routes/news.route.js";
+import swaggerRoute from "./src/routes/swagger.route.js"
 import connectDB from "./src/database/db.js";
 
 import { configDotenv } from "dotenv";
@@ -16,6 +17,7 @@ app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/news", newsRoute);
+app.use("/docs", swaggerRoute);
 
 app.listen(port, () => {
   console.log("Service is running on port " + port);
